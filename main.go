8ab3a40b87ce77d720292eaa44af0c58ac2ea7c7
Bishwas Bhandari <bishwasbh@gmail.com>
2024-10-30 10:28:58 +0545

    chore: human data loaded

--- main.go
+++ main.go
@@ -1,27 +1,79 @@
 package main
 
 import (
+	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/widget"
-	"time"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"image/color"
+	"log"
+	"reflect"
 )
 
-func updateCurrentTime(headTitle *widget.Label) {
-	for {
-		headTitle.SetText(time.Now().Format("Time: 15:04:05"))
-		time.Sleep(1 * time.Second)
-	}
+type human struct {
+	Name           string
+	Age            int
+	Profession     string
+	Gender         string
+	ArrivalDate    string
+	DepartureDate  string
+	Reason         string
+	IsInterstellar bool
+	Inhabitant     string
 }
 
 func main() {
-	a := app.New()
-	w := a.NewWindow("Hello World")
-	w.Resize(fyne.NewSize(400, 200))
-	headTitle := widget.NewLabel("Time: --:--:--")
-	w.SetContent(headTitle)
-	go func() {
-		updateCurrentTime(headTitle)
-	}()
-	w.ShowAndRun()
+	phobe := human{
+		Name:           "Phobe",
+		Age:            25,
+		Profession:     "Software Engineer",
+		Gender:         "Female",
+		ArrivalDate:    "2021-09-01",
+		DepartureDate:  "2021-09-30",
+		Reason:         "Work",
+		IsInterstellar: false,
+		Inhabitant:     "Earth",
+	}
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Human Information")
+	myWindow.Resize(fyne.NewSize(100, 400))
+	humanDisplayUI := getHumanDisplayUI(phobe)
+	content := container.New(layout.NewHBoxLayout())
+	for _, pair := range humanDisplayUI {
+		content.Add(container.New(layout.NewVBoxLayout(), pair.Label, pair.Text))
+	}
+	myWindow.SetContent(content)
+	myWindow.ShowAndRun()
+}
+
+type humanDisplayUI struct {
+	Label *canvas.Text
+	Text  *canvas.Text
+}
+
+func getHumanDisplayUI(human human) []humanDisplayUI {
+	var pairs []humanDisplayUI
+	v := reflect.ValueOf(human)
+	for i := 0; i < v.NumField(); i++ {
+		label := canvas.NewText(v.Type().Field(i).Name, color.White)
+		var text *canvas.Text
+		value := v.Field(i)
+		log.Println(value)
+		switch value.Kind() {
+		case reflect.Int:
+			text = canvas.NewText(fmt.Sprintf("%d", value.Int()), color.White)
+		case reflect.Bool:
+			if value.Bool() {
+				text = canvas.NewText("Yes", color.White)
+			} else {
+				text = canvas.NewText("No", color.White)
+			}
+		default:
+			text = canvas.NewText(value.String(), color.White)
+		}
+		pairs = append(pairs, humanDisplayUI{Label: label, Text: text})
+	}
+	return pairs
 }
